fdf0edf71222480e39037c4b441b225f9e2f5629
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-04-04 13:13:15 +0500

    custom paper created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import CustomLink from "@/components/Link/link";
+import Paper from "@/components/Paper/paper";
 import { palette } from "@/theme/Palette";
 import { Button, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <CustomLink href="/link">
+    <Paper type="light-bg-border">
       <p style={{ padding: "10px", margin: "100px" }}>Hello wrold</p>
-    </CustomLink>
+      <p style={{ padding: "10px", margin: "100px" }}>Hello wrold</p>
+      <p style={{ padding: "10px", margin: "100px" }}>Hello wrold</p>
+      <p style={{ padding: "10px", margin: "100px" }}>Hello wrold</p>
+    </Paper>
   );
 }
--- src/components/Paper/paper.tsx
+++ src/components/Paper/paper.tsx
@@ -1,9 +1,68 @@
-import { Paper } from "@mui/material"
+import { Paper as MuiPaper } from "@mui/material";
+import { FC, ReactNode } from "react";
 
-const Papper = () => {
-    return(
-        <Paper >
-        
-        </Paper>
-    )
+interface PaperProps {
+  type: "light-border" | "dark-border" | "light-bg-border";
+  children: ReactNode;
 }
+
+const Paper: FC<PaperProps> = ({ type, children, ...props }) => {
+  switch (type) {
+    case "light-border":
+      return (
+        <>
+          <MuiPaper
+            {...props}
+            sx={{
+              m: 1,
+              borderRadius: "16px",
+              border: "2px solid rgba(196, 196, 196, 0.60)",
+              background:
+                "linear-gradient(143deg, rgba(255, 255, 255, 0.15) -3.54%, rgba(114, 114, 114, 0.17) 95.15%)",
+              backdropFilter: "blur(20px)",
+            }}
+          >
+            {children}
+          </MuiPaper>
+        </>
+      );
+    case "dark-border":
+      return (
+        <>
+          <MuiPaper
+            {...props}
+            sx={{
+              m: 1,
+              borderRadius: "16px",
+              border: "2px solid var(--Vision-pro-02, #393939)",
+              opacity: "0.5",
+              background:
+                "linear-gradient(143deg, rgba(255, 255, 255, 0.11) -3.54%, rgba(114, 114, 114, 0.13) 95.15%)",
+              backdropFilter: "blur(20px)",
+            }}
+          >
+            {children}
+          </MuiPaper>
+        </>
+      );
+    case "light-bg-border":
+      return (
+        <>
+          <MuiPaper
+            {...props}
+            sx={{
+              borderRadius: "16px",
+              border: "2px solid rgba(196, 196, 196, 0.60)",
+              background:
+                "linear-gradient(143deg, rgba(255, 255, 255, 0.07) -3.54%, rgba(114, 114, 114, 0.08) 95.15%)",
+              backdropFilter: "blur(20px)",
+            }}
+          >
+            {children}
+          </MuiPaper>
+        </>
+      );
+  }
+};
+
+export default Paper;
