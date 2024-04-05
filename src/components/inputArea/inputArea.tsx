1f54c2deb05045f4b7370efa59a8dcc170cecfa4
Aaliyan Asif <93864353+AaliyanAsif@users.noreply.github.com>
2024-04-05 16:27:01 +0500

    input area

---
+++ src/components/inputArea/inputArea.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import { Paper } from "../Paper";
+
+const InputArea = () => {
+  return (
+    <>
+      <Paper type="light-border">
+        <Box>test</Box>
+      </Paper>
+    </>
+  );
+};
+
+export default InputArea;
