dd3356543a701e74cb2cbbef70db97cadae2db10
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-04-09 15:26:35 +0500

    merge and conflict solved

--- src/modules/PreviewDataPage/previewDataPage.tsx
+++ src/modules/PreviewDataPage/previewDataPage.tsx
@@ -31,13 +31,13 @@ const PreviewDataPage: FC = () => {
             <Typography variant="text-md-semibold"> Summary</Typography>
             <br />
             <Typography variant="text-sm-regular">
-              "This query first selects distinct customer IDs, email addresses,
+              This query first selects distinct customer IDs, email addresses,
               and last interaction timestamps from both the Segment and Lytics
               tables, and sums up the total transactions from both tables.
-              <br /> Itthen filters the results based on membership in Wawa's
+              <br /> Itthen filters the results based on membership in Wawas
               loyalty program and whether a purchase or redemption was made in
               the past week. Finally, it merges the two lists, deduplicating
-              line items by Customer ID"
+              line items by Customer ID
             </Typography>
           </Paper>
           <Paper type="light-border"></Paper>
