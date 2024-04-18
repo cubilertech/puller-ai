4d5b17f86c08f950bf865b2a4c1daa243f6397fc
Aaliyan Asif <93864353+AaliyanAsif@users.noreply.github.com>
2024-04-18 11:24:20 +0500

    create alert route

---
+++ src/app/alerts/page.tsx
@@ -0,0 +1,14 @@
+import AppLayout from "@/common/appLayout/appLayout";
+import { Typography } from "@mui/material";
+
+const page = () => {
+  return (
+    <>
+      <AppLayout>
+        <Typography variant="display-2xl-bold">Alerts</Typography>
+      </AppLayout>
+    </>
+  );
+};
+
+export default page;
