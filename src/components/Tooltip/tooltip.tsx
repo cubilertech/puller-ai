ede9c732c2f4c593cc54e584840b55049fd21275
Aaliyan Asif <93864353+AaliyanAsif@users.noreply.github.com>
2024-04-22 17:08:36 +0500

    make tooltip children inline

--- src/components/Tooltip/tooltip.tsx
+++ src/components/Tooltip/tooltip.tsx
@@ -20,82 +20,78 @@ const Tooltip: FC<TooltipProps> = ({
   switch (variant) {
     case "info":
       return (
-        <div>
-          <MuiTooltip
-            placement="top"
-            title={
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "0.5rem",
-                }}
+        <MuiTooltip
+          placement="top"
+          title={
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "0.5rem",
+              }}
+            >
+              <Typography
+                variant="text-md-regular"
+                color={"rgba(230, 230, 230, 1)"}
               >
-                <Typography
-                  variant="text-md-regular"
-                  color={"rgba(230, 230, 230, 1)"}
-                >
-                  {title}
-                </Typography>
-                <Typography variant="text-sm">{description}</Typography>
-              </Box>
-            }
-            {...props}
-          >
-            {children}
-          </MuiTooltip>
-        </div>
+                {title}
+              </Typography>
+              <Typography variant="text-sm">{description}</Typography>
+            </Box>
+          }
+          {...props}
+        >
+          {children}
+        </MuiTooltip>
       );
     case "status":
       return (
-        <div>
-          <MuiTooltip
-            placement="bottom-start"
-            title={
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "0.5rem",
-                }}
+        <MuiTooltip
+          placement="bottom-start"
+          title={
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "0.5rem",
+              }}
+            >
+              {" "}
+              <Typography
+                variant="text-md-semibold"
+                display={"flex"}
+                gap={"1rem"}
               >
-                {" "}
-                <Typography
-                  variant="text-md-semibold"
-                  display={"flex"}
-                  gap={"1rem"}
-                >
-                  <Icon icon="live" /> Live
-                </Typography>
-                <Typography
-                  variant="text-md-semibold"
-                  display={"flex"}
-                  gap={"1rem"}
-                >
-                  <Icon icon="issues" /> Issues
-                </Typography>
-                <Typography
-                  variant="text-md-semibold"
-                  display={"flex"}
-                  gap={"1rem"}
-                >
-                  <Icon icon="blocked" /> Blocked
-                </Typography>
-                <Typography
-                  variant="text-md-semibold"
-                  display={"flex"}
-                  gap={"1rem"}
-                >
-                  <Icon icon="needPermissions" /> Need Permissions
-                </Typography>
-                <Typography variant="text-sm">{description}</Typography>
-              </Box>
-            }
-            {...props}
-          >
-            {children}
-          </MuiTooltip>
-        </div>
+                <Icon icon="live" /> Live
+              </Typography>
+              <Typography
+                variant="text-md-semibold"
+                display={"flex"}
+                gap={"1rem"}
+              >
+                <Icon icon="issues" /> Issues
+              </Typography>
+              <Typography
+                variant="text-md-semibold"
+                display={"flex"}
+                gap={"1rem"}
+              >
+                <Icon icon="blocked" /> Blocked
+              </Typography>
+              <Typography
+                variant="text-md-semibold"
+                display={"flex"}
+                gap={"1rem"}
+              >
+                <Icon icon="needPermissions" /> Need Permissions
+              </Typography>
+              <Typography variant="text-sm">{description}</Typography>
+            </Box>
+          }
+          {...props}
+        >
+          {children}
+        </MuiTooltip>
       );
   }
 };
