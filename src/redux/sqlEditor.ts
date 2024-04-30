72562af1218717789828eb9e43441663adc7d983
Aaliyan Asif <93864353+AaliyanAsif@users.noreply.github.com>
2024-04-30 17:59:10 +0500

    fix alternating in sql editor & optionbar

--- src/redux/sqlEditor.ts
+++ src/redux/sqlEditor.ts
@@ -2,21 +2,37 @@ import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "./store";
 
 export const getSQLEditorOpen = (state: RootState) => state.sqlEditor.open;
+export const getOptionbarOpen = (state: RootState) =>
+  state.sqlEditor.optionbarOpen;
 
 const SQLEditor = createSlice({
   name: "sqlEditor",
   initialState: {
     open: false,
+    optionbarOpen: false,
   },
   reducers: {
     HandleOpenSQL: (state) => {
       state.open = true;
+      state.optionbarOpen = false;
     },
     HandleCloseSQL: (state) => {
       state.open = false;
     },
+    HandleOpenOptionbar: (state) => {
+      state.open = false;
+      state.optionbarOpen = true;
+    },
+    HandleCloseOptionbar: (state) => {
+      state.optionbarOpen = false;
+    },
   },
 });
 
-export const { HandleOpenSQL, HandleCloseSQL } = SQLEditor.actions;
+export const {
+  HandleOpenSQL,
+  HandleCloseSQL,
+  HandleCloseOptionbar,
+  HandleOpenOptionbar,
+} = SQLEditor.actions;
 export default SQLEditor.reducer;
