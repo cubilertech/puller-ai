bddcab339c7dbce2e9bcd33a47ea3760a7074f44
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-05-06 19:53:03 +0500

    all done

--- src/modules/AdvancedPage/advancedPage.tsx
+++ src/modules/AdvancedPage/advancedPage.tsx
@@ -1,9 +1,18 @@
+"use client";
 import { PageHeader } from "@/components/PageHeader";
-
 import { RectangleCard } from "@/components/RectangleCard";
+import { AlertModal } from "@/modals/AlertModal";
+import { CURRENT_MODE, MODES } from "@/utils/constants";
 import { Box } from "@mui/material";
+import { useState } from "react";
 
 const AdvancedPage = () => {
+  const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const handleClick = () => {
+    if (CURRENT_MODE === MODES.PILOT) {
+      setIsOpenAlert(true);
+    }
+  };
   return (
     <Box p={"1rem"} display={"flex"} flexDirection={"column"} gap={"2rem"}>
       <PageHeader title="Advanced" />
@@ -13,10 +22,26 @@ const AdvancedPage = () => {
         gap={"2rem"}
         flexDirection={{ md: "row", xs: "column" }}
       >
-        <RectangleCard icon="apiKey" title="Advanced Analytics" />
-        <RectangleCard icon="connectApps" title="Connect to App or Dashboard" />
-        <RectangleCard icon="cpu" title="Connect to AI Model" />
+        <RectangleCard
+          icon="apiKey"
+          title="Advanced Analytics"
+          onClick={() => handleClick()}
+        />
+        <RectangleCard
+          icon="connectApps"
+          title="Connect to App or Dashboard"
+          onClick={() => handleClick()}
+        />
+        <RectangleCard
+          icon="cpu"
+          title="Connect to AI Model"
+          onClick={() => handleClick()}
+        />
       </Box>
+      <AlertModal
+        open={isOpenAlert}
+        handleClose={() => setIsOpenAlert(false)}
+      />
     </Box>
   );
 };
