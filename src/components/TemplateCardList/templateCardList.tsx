a4d4b5f375a71a8807c29ee97ce8eacbaea3f73e
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-05-08 16:36:40 +0500

    lint, build issues and ui fixing

--- src/components/TemplateCardList/templateCardList.tsx
+++ src/components/TemplateCardList/templateCardList.tsx
@@ -1,9 +1,10 @@
 // import { ACTIVE_TYPES } from "@/utils/constants";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { FC } from "react";
 // import { TEMPLATE_PRIVATE_DATA, TEMPLATE_PUBLIC_DATA } from "@/utils/data";
 import { TemplateCard } from "../TemplateCard";
 import { Prompt } from "@/utils/types";
+import { palette } from "@/theme/Palette";
 interface TemplateCardListProps {
   isActive: string;
   pulls: Prompt[];
@@ -18,11 +19,25 @@ const TemplateCardList: FC<TemplateCardListProps> = ({ isActive, pulls }) => {
         scrollbarWidth: "none",
       }}
     >
-      {pulls?.length <= 0
-        ? `No ${isActive} pulls available.`
-        : pulls?.map((pull, index) => (
-            <TemplateCard key={index} index={index} card={pull} />
-          ))}
+      {pulls?.length <= 0 ? (
+        <Typography
+          sx={{
+            color: palette.color.gray[500],
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "100%",
+          }}
+          variant="text-lg-regular"
+        >
+          No {isActive} pulls available.
+        </Typography>
+      ) : (
+        pulls?.map((pull, index) => (
+          <TemplateCard key={index} index={index} card={pull} />
+        ))
+      )}
     </Box>
   );
 };
