1d3312c21d6304ba8559ffc2aae0dffebeae4c0e
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-05-21 17:48:16 +0500

    upload image

--- src/components/UplaodBox/uploadBox.tsx
+++ src/components/UplaodBox/uploadBox.tsx
@@ -1,9 +1,15 @@
 import { Box, Input } from "@mui/material";
 import { UploadCard } from "../UploadCard";
+import { FC } from "react";
 
 const data = [1, 2, 3];
 
-const UploadBox = () => {
+interface UplaodBoxProps {
+  name?: string;
+  size?: number;
+}
+
+const UploadBox: FC<UplaodBoxProps> = ({ name, size }) => {
   return (
     <Box
       display={"grid"}
@@ -11,20 +17,20 @@ const UploadBox = () => {
       gap={"1.4rem"}
       mb={"2rem"}
     >
-      {data.map((item, index) => (
-        <Box display={"flex"} flexDirection={"column"} gap={"1rem"} key={index}>
-          <UploadCard />
-          <Input
-            disableUnderline
-            placeholder="Add additional context here"
-            sx={{
-              border: "2px solid rgba(196, 196, 196, 0.6)",
-              borderRadius: "5px",
-              padding: "0.1rem 0.5rem",
-            }}
-          />
-        </Box>
-      ))}
+      {/* {data.map((item, index) => ( */}
+      <Box display={"flex"} flexDirection={"column"} gap={"1rem"}>
+        <UploadCard name={name} size={size} />
+        <Input
+          disableUnderline
+          placeholder="Add additional context here"
+          sx={{
+            border: "2px solid rgba(196, 196, 196, 0.6)",
+            borderRadius: "5px",
+            padding: "0.1rem 0.5rem",
+          }}
+        />
+      </Box>
+      {/* ))} */}
     </Box>
   );
 };
--- src/modules/UploadRetrieverPage/uploadRetrieverPage.tsx
+++ src/modules/UploadRetrieverPage/uploadRetrieverPage.tsx
@@ -10,18 +10,29 @@ import { AlertModal } from "@/modals/AlertModal";
 import { isPilotMode } from "@/utils/constants";
 import { Box, Input, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 const UploadRetrieverPage = () => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploadStatus, setUploadStatus] = useState<string>("");
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
 
   const handleUploadDocs = () => {
     if (isPilotMode) {
       setIsOpenAlert(true);
     } else {
-      setIsOpen(!isOpen);
+      if (fileInputRef.current) {
+        fileInputRef.current.click();
+      }
     }
   };
 
@@ -32,6 +43,14 @@ const UploadRetrieverPage = () => {
       router.push("/retrievers/feedback");
     }
   };
+  useEffect(() => {
+    if (selectedFile) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }, [selectedFile]);
+  console.log(selectedFile, "selectedFile");
   return (
     <Box
       sx={{
@@ -100,8 +119,15 @@ const UploadRetrieverPage = () => {
               alignItems: "center",
               flexDirection: "column",
               gap: "1rem",
+              cursor: "pointer",
             }}
           >
+            <input
+              type="file"
+              onChange={handleFileChange}
+              ref={fileInputRef}
+              style={{ display: "none" }}
+            />
             <Icon icon="cloudUpload" width={40} height={40} />
             <Typography variant="text-sm-medium">
               <Typography variant="text-sm-bold">Click to upload</Typography> or
@@ -119,7 +145,7 @@ const UploadRetrieverPage = () => {
             />
           ) : (
             // Upload Cards & Inputs
-            <UploadBox />
+            <UploadBox name={selectedFile?.name} size={selectedFile?.size} />
           )}
 
           {/* Back & Create Buttons */}
